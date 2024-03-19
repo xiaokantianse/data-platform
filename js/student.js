b8caf5f86577779716bfac7ebc01db6c0c2c902e
xiaokantianse <1440742690@qq.com>
2024-03-19 21:35:09 +0800

    省市区联动完成

--- js/student.js
+++ js/student.js
@@ -28,3 +28,58 @@ logout()
   }).join('')
  }
  render()
+
+ // 初始化模态框
+const modalDom = document.querySelector('#modal')
+const myModal = new bootstrap.Modal(modalDom)
+document.querySelector('#openModal').addEventListener('click', e=>{
+  myModal.show()
+})
+
+// 省市区联动函数抽取
+
+const selectLocation = async ()=>{
+  const provinceDom = document.querySelector('[name=province]')
+  const cityDom = document.querySelector('[name=city]')
+  const areaDom = document.querySelector('[name=area]')
+  
+  // 默认渲染省份出来
+  const {list} = await axios.get('/api/province')
+  // console.log(list);
+ provinceDom.innerHTML +=  list.map(ele => {
+    return `<option value="${ele}">${ele}</option>`
+  }).join('')
+  
+  // 根据省渲染市
+  // 身份绑定change事件
+  provinceDom.addEventListener('change', async e=>{
+    // 要清空之前的内容
+    cityDom.innerHTML = '<option value="">--城市--</option>'
+   const {list} = await axios.get('/api/city',{params:{
+      pname:e.target.value
+    }})
+    cityDom.innerHTML += list.map(item =>{
+      return `<option value="${item}">${item}</option>`
+    }).join('')
+  })
+
+  // 地区切换
+  cityDom.addEventListener('change', async e=>{
+    areaDom.innerHTML = '<option value="">--地区--</option>'
+    const {list} = await axios.get('/api/area', {params:{
+      pname:provinceDom.value,
+      cname: e.target.value,
+    }})
+
+   areaDom.innerHTML +=  list.map(item =>{
+      return `<option value="${item}">${item}</option>`
+    }).join('')
+  })
+
+
+  
+
+
+
+}
+selectLocation()
